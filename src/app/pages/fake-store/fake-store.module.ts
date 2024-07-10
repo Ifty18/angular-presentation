b853d383af5cd9d5e1176c58de97fbb0dfbc1932
Razvan Iftimoaia <razvaniftimoaia@Razvans-MacBook-Pro.local>
2024-07-11 01:37:55 +0300

    added filter button + mat-cards

--- src/app/pages/fake-store/fake-store.module.ts
+++ src/app/pages/fake-store/fake-store.module.ts
@@ -1,9 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FakeStoreRoutingModule } from './fake-store-routing.module';
-import { ProductDetailsComponent } from './store/pages/product-details/product-details.component';
-import { StoreProductComponent } from './store/pages/store-product/store-product.component';
-import { StoreComponent } from './store/store.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FakeStoreRoutingModule } from "./fake-store-routing.module";
+import { ProductDetailsComponent } from "./store/pages/product-details/product-details.component";
+import { StoreProductComponent } from "./store/pages/store-product/store-product.component";
+import { StoreComponent } from "./store/store.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   declarations: [
@@ -11,7 +14,13 @@ import { StoreComponent } from './store/store.component';
     StoreProductComponent,
     ProductDetailsComponent,
   ],
-  imports: [CommonModule, FakeStoreRoutingModule],
+  imports: [
+    CommonModule,
+    FakeStoreRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+  ],
   exports: [],
 })
 export class FakeStoreModule {}
--- src/app/pages/fake-store/store/store.component.ts
+++ src/app/pages/fake-store/store/store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, map, Observable, Observer } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -31,5 +32,32 @@ export class StoreComponent implements OnInit {
         image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
       })
       .subscribe((response) => (this.addedProduct = response));
+  }
+
+  filterProducts(): void {
+    const customFilterObservable = Observable.create((observer:Observer<Product>) => {
+      this.products.forEach(product => {
+        observer.next(product);
+      });
+      observer.complete();
+    })
+
+    let auxProducts:Product[] = [];
+
+    customFilterObservable
+      .pipe(filter((product:Product) => {
+        if (product.price <= 50) {
+          return true;
+        }
+        return false;
+      }))
+      .subscribe((product:Product) => {
+        auxProducts.push(product);
+      }, (error: Error) => {
+        console.log(error);
+      }, () => {
+        console.log("completed!");
+        this.products = auxProducts;
+      });
   }
 }
